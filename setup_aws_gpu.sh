47b2401be2beead891ea6c48ff96e268666ca959
Dinç Çiftçi <dincciftci@users.noreply.github.com>
2016-02-11 15:24:03 -0500

    Added script to set up the amazon machine more easily

---
+++ setup_aws_gpu.sh
@@ -0,0 +1,8 @@
+sudo mkdir /data
+sudo mount /dev/xvdf /data
+
+sudo apt-get update
+sudo apt-get install git
+sudo pip install -r https://raw.githubusercontent.com/Lasagne/Lasagne/v0.1/requirements.txt
+sudo pip install Lasagne==0.1
+sudo pip install -U scikit-learn
