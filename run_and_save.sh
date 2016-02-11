f7aa3d0017e83c917f7d6029fbbd46be24e39219
Dinç Çiftçi <dincciftci@users.noreply.github.com>
2016-02-11 15:11:00 -0500

    Reverting learning rate change, fixing bash script

--- run_and_save.sh
+++ run_and_save.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo nohup python -u train_net.py -c sx3_ffc -s state_ffc > ffc_output.txt
-cp ffc_output.txt state_ffc.npy /data
+sudo cp ffc_output.txt state_ffc.npy /data
 sudo nohup python -u train_net.py -c sx3_ffc_b5 -s state_ffc_b5 > ffc_b5_output.txt
-cp ffc_b5_output.txt state_ffc_b5.npy /data
+sudo cp ffc_b5_output.txt state_ffc_b5.npy /data
 sudo nohup python -u train_net.py -c sx3_fffc -s state_fffc > fffc_output.txt
-cp fffc_output.txt state_fffc.npy /data
+sudo cp fffc_output.txt state_fffc.npy /data
 sudo shutdown -h now
