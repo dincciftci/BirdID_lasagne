38ec79818a2bc27f65ad55760992782658a6f04c
Dinç Çiftçi <dincciftci@users.noreply.github.com>
2016-03-20 15:41:32 -0400

    Change sklearn install because of version differences

--- setup_aws_gpu.sh
+++ setup_aws_gpu.sh
@@ -5,7 +5,8 @@ sudo apt-get update
 sudo apt-get -y install git
 sudo pip install -r https://raw.githubusercontent.com/Lasagne/Lasagne/v0.1/requirements.txt
 sudo pip install Lasagne==0.1
-sudo apt-get  -y install python-sklearn
+#sudo apt-get  -y install python-sklearn
+sudo pip install -U scikit-learn
 git clone https://github.com/HIPS/Spearmint.git
 cd Spea*
 python setup.py install --user
