a2e557956ad9c9ff9e00d5267109d9896a05b64e
Dinç Çiftçi <dincciftci@users.noreply.github.com>
2016-03-20 15:18:02 -0400

    Changes to gpu setup

--- setup_aws_gpu.sh
+++ setup_aws_gpu.sh
@@ -5,5 +5,11 @@ sudo apt-get update
 sudo apt-get -y install git
 sudo pip install -r https://raw.githubusercontent.com/Lasagne/Lasagne/v0.1/requirements.txt
 sudo pip install Lasagne==0.1
-sudo apt-get install python-sklearn
-# needs to install (simple)spearmint
+sudo apt-get  -y install python-sklearn
+git clone https://github.com/HIPS/Spearmint.git
+cd Spea*
+python setup.py install --user
+cd ..
+git clone https://github.com/craffel/simple_spearmint.git
+cd simpl*
+python setup.py install --user
